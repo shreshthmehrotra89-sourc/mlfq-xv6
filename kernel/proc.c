277622f8b08d7d7629e4adcfc57710fcd282f0aa
shreshth <shreshthmehrotra89@gmail.com>
2026-09-08 11:37:49 +0530

    setup

--- kernel/proc.c
+++ kernel/proc.c
@@ -124,6 +124,8 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
+  p->queue = 0;
+  p->ticks_in_slice = 0;
 
   // Allocate a trapframe page.
   if ((p->trapframe = (struct trapframe *)kalloc()) == 0) {
--- kernel/proc.h
+++ kernel/proc.h
@@ -101,4 +101,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  // MLFQ bookkeeping
+  int queue;
+  int ticks_in_slice;
 };
